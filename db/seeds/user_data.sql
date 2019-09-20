1fcc3b22aff15ce694ab2fef1b5b87f3c0e2c8b5
Kevin Convery <kevinconvery@gmail.com>
2019-09-20 19:07:20 +0000

    modify seeds to include new user_id keys

--- db/seeds/user_data.sql
+++ db/seeds/user_data.sql
@@ -1,6 +1,7 @@
-INSERT INTO users (name, email, password, phone_number)
+INSERT INTO users (user_id, name, email, password, phone_number)
 VALUES
 (
+  'AAAAAA',
   'Bob Jones',
   'bobjones@bob.com',
   -- bcrypt hashed version of the word 'password' for all users
@@ -8,24 +9,28 @@ VALUES
   6476378535
 ),
 (
+  'BBBBBB',
   'Cindy Jones',
   'cindyjones@bob.com',
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   4164445556
 ),
 (
+  'CCCCCC',
   'Reggie Brown',
   'rbrown@gmail.com',
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   9053323336
 ),
 (
+  'DDDDDD',
   'Andrea Smith',
   'andrea@hardtack.com',
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
   4168888054
 ),
 (
+  'EEEEEE',
   'Frank Black',
   'frankblack@hotmail.com',
   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
