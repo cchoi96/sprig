d2568c904fa1026660c86240607246ae30b65f04
Kevin Convery <kevinconvery@gmail.com>
2019-09-20 18:28:14 +0000

    add menu items data

---
+++ db/seeds/menu_items_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO menu_items (restaurant_id, name, cost, is_available)
+VALUES
+(
+  1,
+  "French Fries",
+  299,
+  true
+),
+(
+  1,
+  "Hamburger",
+  799,
+  true
+),
+(
+  1,
+  "Halibut and Chips",
+  1029,
+  true
+),
+(
+  1,
+  "Sundae",
+  459,
+  true
+);
