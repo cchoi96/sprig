2c23ad42b7ae533a4734f09d5b0a1dcdf69b2fca
Kevin Convery <kevinconvery@gmail.com>
2019-09-23 01:34:56 +0000

    add new menu items to menu_items seed

--- db/seeds/menu_items_data.sql
+++ db/seeds/menu_items_data.sql
@@ -1,38 +1,108 @@
-INSERT INTO menu_items (restaurant_id, name, cost_in_cents, is_available)
+INSERT INTO menu_items (restaurant_id, name, description, cost_in_cents, is_available)
 VALUES
 (
   1,
   'French Fries',
+  'Crispy hand-cut Ontario potatoes made-to-order.',
   299,
   true
 ),
 (
   1,
   'Hamburger',
+  'A flame-broiled hamburger done over a coal-fired barbeque.',
   799,
   true
 ),
 (
   1,
   'Halibut and Chips',
+  'Fresh Atlantic halibut served with tartar sauce and lemon, along with a side order of our french fries.',
   1029,
   true
 ),
 (
   1,
+  'Grilled Cheese Sandwich',
+  'Toasted oozy cheese sandwich, served with your choice of soup.',
+  669,
+  true
+),
+(
+  1,
   'Chocolate Sundae',
-  459,
+  'Two scoops of vanilla ice cream topped with our made-from-scratch chocolate sauce and strawberries. Whipped cream optional.',
+  749,
+  true
+),
+(
+  1,
+  'Coke',
+  'Can of Coke',
+  129,
   true
 ),
 (
   2,
   'Pepperoni Pizza',
+  'Cured pepperoni, San Marzano tomato sauce and mozzarella di bufala cheese on a thin-crust pizza. Decadent.',
   999,
   true
 ),
 (
   2,
-  'Veal Panini',
-  879,
+  'Eggplant Parmigiana Panini',
+  'Breaded eggplant served on a fresh ciabatta bun, with your choice of toppings.',
+  1179,
+  true
+),
+(
+  2,
+  'Caesar Salad',
+  'This dish was actually invented in Mexico, but don''t let that dissuade you from ordering it! We''re confident you''ll enjoy it as our dressing is made in-house.',
+  749,
+  true
+),
+(
+  2,
+  'Pasta al Forno',
+  'Baked penne with cheese and tomato sauce with Italian hot sausage. Nonna''s special recipe.',
+  999,
+  true
+),
+(
+  3,
+  'Brisket Sandwich',
+  'Freshly out of the barbeque, cut to order. Served on a Portuguese bun with coleslaw.',
+  1199,
+  true
+),
+(
+  3,
+  'Pulled Pork',
+  'Slow-rolled this one! Twelve hours in our oven and lovingly basted with our bourbon sauce until it''s ready to pull and serve. Served on a Portuguese bum with coleslaw',
+  999,
+  true
+),
+(
+  3,
+  'Rice and Beans',
+  'White rice and red kidney beans, great for eating alongside our meats a la carte.',
+  559,
+  true
+),
+(
+  3,
+  'Half Chicken and Rice',
+  'Just as it says. Served with rice and beans, one of our homemade dinner rolls and your choice of side salad on pickup.',
+  1199,
+  true
+),
+(
+  3,
+  'Full Chicken and Rice',
+  'For two or more. Includes two dinner rolls, a large salad, our roasted potatoes and rice and peas.',
+  2199,
   true
 );
+
