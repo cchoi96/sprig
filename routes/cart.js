8d5fe50b446ba1af636b31f96d8173a0f543883c
Kevin Convery <kevinconvery@gmail.com>
2019-09-24 19:07:15 +0000

    modify cart to print text with order number

--- routes/cart.js
+++ routes/cart.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const router = express.Router();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const phoneNumber = process.env.TWILIO_NUMBER;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 // const { generateRandomSMSId } = require('../public/scripts/helpers');
 
@@ -71,23 +71,18 @@ module.exports = (db) => {
     const restaurantAndItemIds = JSON.parse(req.body.orderData);
     console.log(restaurantAndItemIds);
 
-    // client.messages
-    //   .create({
-    //     body: 'You have a new order! The order is ',
-    //     from: phoneNumber,
-    //     to: '+17788469842'
-    //   })
-    //   .then(message => console.log(message.sid));
+
 
     let restaurantId = restaurantAndItemIds.restaurantId;
-    let orderIdQuery = `INSERT INTO orders(customer_id, restaurant_id, sms_code)
-                             VALUES($1, $2, $3)
+    let orderIdQuery = `INSERT INTO orders(customer_id, restaurant_id)
+                             VALUES($1, $2)
                              RETURNING *`;
 
-    const values = [req.session.user_id, restaurantId, 9999];
+    const values = [req.session.user_id, restaurantId];
     db
       .query(orderIdQuery, values)
       .then(orderResponse => {
+        let queries = [];
         let orderId = orderResponse.rows[0].id;
         // Checks if quantity = 0. If not, it pushes it into the order_items table.
         for (let orderIndex = 0; orderIndex < restaurantAndItemIds.itemId.length; orderIndex++) {
@@ -96,14 +91,24 @@ module.exports = (db) => {
             VALUES($1, $2, $3)
             RETURNING *`;
             let orderItemValues = [orderId, restaurantAndItemIds.itemId[orderIndex], restaurantAndItemIds.quantity[orderIndex]];
-
-            db
-              .query(orderItemQuery, orderItemValues)
-              .then(insertedOrderData => {
-              })
+            queries.push(db.query(orderItemQuery, orderItemValues));
           }
         }
-      })
+        Promise.all(queries)
+          .then(insertedOrderData => {
+            let messageOrderId = insertedOrderData[0].rows[0].order_id;
+            client.messages.create({
+              body: `You have a new message!\nPlease check for order# ${messageOrderId} in your Sprig account!`,
+              from: `+1${phoneNumber}`,
+              to: `+16476378535`
+            })
+            .then(message => message.sid)
+            .catch(err => console.error(err));
+          })
+          .catch(err => {
+            console.log(`ERROR: ${err}`);
+          })
+        })
       .catch(err => {
         console.error(err);
       })
