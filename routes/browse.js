fad24240c9d926e5126c1c822245a45dc95fc49c
Chris Choi <chrischoi96@gmail.com>
2019-10-05 19:36:39 +0000

    some api key changes

--- routes/browse.js
+++ routes/browse.js
@@ -1,55 +1,57 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const router = express.Router();
+require("dotenv").config();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.use(cookieSession({
-  name: 'user_id',
-  keys: ['id']
-}));
+router.use(
+  cookieSession({
+    name: "user_id",
+    keys: ["id"]
+  })
+);
 
-module.exports = (db) => {
+module.exports = db => {
   // Data object to be passed into EJS
   let data = {
-    user: '',
+    user: "",
     error: {},
-    restaurants: '',
-    restaurant: '',
-    orderInfo: '',
-    email: '',
+    restaurants: "",
+    restaurant: "",
+    orderInfo: "",
+    email: ""
   };
 
   // @route   GET /browse/cart
   // @ desc   Receives information sent from individual restaurants and pushes it to cart.ejs, only if user is logged in
-  router.post('/cart', (req, res) => {
+  router.post("/cart", (req, res) => {
     if (req.session.user_id) {
       data.orderInfo = req.body;
       data.image_url = req.session.image_url;
-      res.render('cart', data);
+      res.render("cart", data);
     } else {
-      res.redirect('/login');
+      res.redirect("/login");
     }
   });
 
   // @route   GET /browse/:restaurant_id
   // @ desc   Render browse page for individual restaurant
-  router.get('/:restaurant_id', (req, res) => {
-    const restaurantName = req.params.restaurant_id.split('%20').join(' ');
+  router.get("/:restaurant_id", (req, res) => {
+    const restaurantName = req.params.restaurant_id.split("%20").join(" ");
     const query = `SELECT *
                    FROM restaurants
                    JOIN menu_items ON restaurants.id = menu_items.restaurant_id
                    WHERE restaurants.name LIKE $1`;
     const values = [restaurantName];
-    db
-      .query(query, values)
+    db.query(query, values)
       .then(restaurantData => {
         data.restaurantName = restaurantName;
         data.restaurantUrlName = req.params.restaurant_id;
         data.restaurantData = restaurantData.rows;
         data.image_url = req.session.image_url;
-        res.render('individual_restaurant', data);
+        res.render("individual_restaurant", data);
       })
       .catch(err => {
         console.log(err);
@@ -58,26 +60,24 @@ module.exports = (db) => {
 
   // @route   GET /browse
   // @ desc   Render browse page
-  router.get('/', (req, res) => {
+  router.get("/", (req, res) => {
     data.email = req.session.email;
     console.log(req.session.email);
     data.user = req.session.user_id;
-    data.apiKey = `AIzaSyAmULEGIDZktxhRsLU6-DH4hrsiz4jiayQ`;
+    data.apiKey = process.env.MAPS_API_KEY;
     const query = `SELECT * FROM restaurants`;
-    db
-      .query(query)
+    db.query(query)
       .then(restaurantData => {
         data.restaurants = restaurantData.rows;
         data.image_url = req.session.image_url;
-        res.render('browse', data);
+        res.render("browse", data);
       })
       .catch(err => console.log(err));
   });
 
   // @route   POST /browse
   // @ desc   Search
-  router.post('/', (req, res) => {
-  });
+  router.post("/", (req, res) => {});
 
   return router;
 };
